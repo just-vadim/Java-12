ce32eea6c21e4ecb577970fcba70d8e5085f1a54
Vadim <vadim.kurn@gmail.com>
2020-10-14 17:56:29 +0300

    ProductManager.java added

---
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -0,0 +1,25 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+    private ProductRepository repository;
+
+    public ProductManager(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public void add(Product item) {
+        repository.save(item);
+    }
+
+    public Product[] searchBy(String text) {
+        return null;
+    }
+
+    public boolean matches(Product product, String search) {
+        // ваш код
+        return false;
+    }
+}
